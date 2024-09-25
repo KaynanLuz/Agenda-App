dd424772a91fda0cbb1d0c72a6619151f25aa38a
KaynanLuz <kaynanhp@hotmail.com>
2024-09-25 14:58:13 -0300

    Chave dominio firebase alterada.

--- src/firebaseConection.js
+++ src/firebaseConection.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCR6zs1uCbD9aPd9QP2UE8hyhZ943LS5Gc",
-  authDomain: "appcontatos-817e7.firebaseapp.com",
-  projectId: "appcontatos-817e7",
-  storageBucket: "appcontatos-817e7.appspot.com",
-  messagingSenderId: "352501288153",
-  appId: "1:352501288153:web:11412ded1281ef9e0882c8",
-  measurementId: "G-1Y7NVCGPHQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
   const firebaseApp = initializeApp(firebaseConfig);
